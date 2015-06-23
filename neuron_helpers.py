cf31ba0b4dd8fc11412236ce9e78d30a80f2f119
George Tzoganakis <george.tzoganakis@appliedbrainresearch.com>
2015-06-22 20:06:49 -0400

    Add helper functions

--- neuron_helpers.py
+++ neuron_helpers.py
@@ -1,4 +1,18 @@
 def step_function(x):
     return 1 if x >= 0 else 0
 
+# Because it is discontinouous, we can't use the above
+# function to train a neural network
 
+def sigmoid(t):
+    return 1 / (1 + math.exp(-t))
+
+# We can use combinations of perceptrons to create
+# various logic gates
+
+def perceptron_output(weights, bias, x):
+    calculation = dot(weights, x) + bias
+    return step_function(calculation)
+
+def neuron_output(weights, inputs):
+    return sigmoid(dot(weights, inputs))
